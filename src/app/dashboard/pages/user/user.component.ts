c3c3e529c07667fdbec3de06b09024cd2ced1aa3
jjmmarcos <javierbulevar8@gmail.com>
2024-01-22 22:01:01 +0100

    files and folders created

---
+++ src/app/dashboard/pages/user/user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html'
+})
+export class UserComponent {
+
+}
---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.css'
+})
+export class SidemenuComponent {
+
+}
