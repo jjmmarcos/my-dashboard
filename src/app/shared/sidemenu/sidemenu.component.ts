1ea6182c4eea2e4a4fb899c7791437bb18ccf5ca
jjmmarcos <javierbulevar8@gmail.com>
2024-01-24 23:02:27 +0100

    dashboard with tailwind

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
+  selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
 export class SidemenuComponent {
+  public menuItems = routes
+  .map(route => route.children ?? [])
+  .flat()
+  .filter(route => route && route.path)
+  .filter(route => !route.path?.includes(':'));
 
+  constructor() {
+    // const dashboardRoute = routes
+    //   .map(route => route.children ?? [])
+    //   .flat()
+    //   .filter(route => route && route.path)
+    //   .filter(route => !route.path?.includes(':'))
+    // console.log(dashboardRoute);
+  }
 }
